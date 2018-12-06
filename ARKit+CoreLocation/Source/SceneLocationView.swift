b2f097cc267709d9583ad55f9f0c6269b276e3ff
paddoum <pierre@bear2b.com>
2018-12-06 15:25:21 +0100

    Fix issue with iteration through TrackingState Cases

--- ARKit+CoreLocation/Source/SceneLocationView.swift
+++ ARKit+CoreLocation/Source/SceneLocationView.swift
@@ -115,7 +115,7 @@ public class SceneLocationView: ARSCNView, ARSCNViewDelegate {
     
     public func run() {
         // Create a session configuration
-        let configuration = ARWorldTrackingSessionConfiguration()
+        let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = .horizontal
         
         if orientToTrueNorth {
@@ -474,14 +474,15 @@ public class SceneLocationView: ARSCNView, ARSCNViewDelegate {
             print("camera did change tracking state: limited, insufficient features")
         case .limited(.excessiveMotion):
             print("camera did change tracking state: limited, excessive motion")
-        case .limited(.none):
-            print("camera did change tracking state: limited, no reason")
         case .limited(.initializing):
             print("camera did change tracking state: limited, initializing")
         case .normal:
             print("camera did change tracking state: normal")
         case .notAvailable:
             print("camera did change tracking state: not available")
+        case .limited(.relocalizing):
+            print("camera did change tracking state: relocalizing")
+        
         }
     }
 }
